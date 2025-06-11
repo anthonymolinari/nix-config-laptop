44d77500977cc7c9a4b9d2ea4e9e6510cec7f61d
anthonymolinari <amolinari017@gmail.com>
2025-06-11 15:40:26 -0700

    zsh config

---
+++ nix_modules/zsh.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, ... }: {
+    programs.zsh = {
+        enable = true;
+        enableCompletion = true;
+        autosuggestion.enable = true;
+        syntaxHighlighting.enable = true;
+
+        oh-my-zsh = {
+            enable = true;
+            plugins = [ "git" ];
+            theme = "";
+        };
+
+        plugins = [
+            {
+                name = "zsh-nix-shell";
+                file = "nix-shell.plugin.zsh";
+                src = pkgs.fetchFromGitHub {
+                    owner = "chisui";
+                    repo = "zsh-nix-shell";
+                    rev = "v0.8.0";
+                    sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+                };
+            }
+        ];
+
+        shellAliases = {
+            update = "home-manager switch";
+            edit = "nvim ~/.config/home-manager";
+        };
+
+        history = {
+            size = 10000;
+            path = "${config.xdg.dataHome}/zsh/history";
+        };
+    };
+}
