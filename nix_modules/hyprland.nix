22785e6d2b3e556b54334dd482217cbf2d554886
anthonymolinari <amolinari017@gmail.com>
2025-06-11 15:40:53 -0700

    add bindings

--- nix_modules/hyprland.nix
+++ nix_modules/hyprland.nix
@@ -9,6 +9,14 @@
         monitor = "eDP-1, 1920x1080@60, 0x0, 1.0";
         "$mod" = "SUPER";
         exec-once="waybar";
+        bindel = [
+            ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
+            ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+            ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+            ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+            ",XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 5%+"
+            ",XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-"
+        ];
         bind = [
             "$mod, RETURN, exec, kitty"
             "$mod, C, killactive,"
@@ -32,7 +40,7 @@
                 let ws = i + 1;
 	            in [
 	                "$mod, code:1${toString i}, workspace, ${toString ws}"
-	            "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+	                "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
 	            ]
             )
             9)
